6dcb111ca3c3b294e32fc65971bdc1492790f9be
Pratham0119 <prathamchavan177@gmail.com>
2026-05-12 14:57:59 +0200

    Add setup.py configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="OncoScanAI",
+    version="0.0.1",
+    author="Pratham Chavan",
+    author_email="prathamchavan177@gmail.com",
+    packages=find_packages()
+)
